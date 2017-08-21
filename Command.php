4d7d0f36aa6b91b14a71e03428b4a4cedf9327c6
Khaled098YT <31197892+Khaled098YT@users.noreply.github.com>
2017-08-21 05:39:47 +0200

    Update Command.php

--- Command.php
+++ Command.php
@@ -3,15 +3,15 @@
 namespace SomeNamespace;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
+use pocketmine\command\Command; // command use
+use pocketmine\command\CommandSender; // Sender is the player
 
 class Command extends PluginBase{
 	
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 		switch($cmd->getName()){
-			case 'hi':
-			  $sender->sendMessage("Hello Player");
+			case 'hi': // set command as hi
+			  $sender->sendMessage("Hello Player"); // send message Hello Player, Soon i will make more things than that
 		}
 	}
 }
